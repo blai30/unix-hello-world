4d4fa1e4eb476f0c75d04ad37d554490d5b7b6ad
Brian Lai <brianlai30@outlook.com>
2019-02-04 13:26:41 -0800

    Added if statements to catch errors for bytes formatted and written

--- hello_world.c
+++ hello_world.c
@@ -8,11 +8,17 @@
 int main(int argc, char const *argv[]) {
     char msg[128];
     int bytes_formatted, bytes_written;
+
     bytes_formatted = sprintf(msg, "CSC 415, This program has been written by %s!\n", NAME);
-    bytes_written = write(4, msg, bytes_formatted);
+    if (bytes_formatted < 0) {
+        perror("Write has failed");
+        return -43;
+    }
 
+    bytes_written = write(1, msg, bytes_formatted);
     if (bytes_written < 0) {
-
+        perror("Write has failed");
+        return -43;
     }
 
 	return 0;
