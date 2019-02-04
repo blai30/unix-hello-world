f02160b6a0e4a05279fa6c69ce3b30a97237ba60
Brian Lai <brianlai30@outlook.com>
2019-02-04 13:01:53 -0800

    Added char msg, define NAME, and sprintf

--- hello_world.c
+++ hello_world.c
@@ -3,11 +3,11 @@
 #include <stdlib.h>
 //You may also add more include directives as well.
 
+#define NAME "Brian"
 
-
-int
-main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
+    char msg[120];
+    sprintf(msg, "CSC 415, This program has been written by %s!", NAME);
 
 	return 0;
 }
