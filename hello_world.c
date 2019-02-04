1f1d6a4d5730c14ef484f478307b5e9ce3e593d5
Brian Lai <brianlai30@outlook.com>
2019-02-04 13:22:42 -0800

    Added bytes formatted and bytes written

--- hello_world.c
+++ hello_world.c
@@ -6,8 +6,14 @@
 #define NAME "Brian"
 
 int main(int argc, char const *argv[]) {
-    char msg[120];
-    sprintf(msg, "CSC 415, This program has been written by %s!", NAME);
+    char msg[128];
+    int bytes_formatted, bytes_written;
+    bytes_formatted = sprintf(msg, "CSC 415, This program has been written by %s!\n", NAME);
+    bytes_written = write(4, msg, bytes_formatted);
+
+    if (bytes_written < 0) {
+
+    }
 
 	return 0;
 }
